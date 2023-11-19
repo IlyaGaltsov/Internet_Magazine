cebe5c2e3b849ca1c9cdd22631ecad285074663b
Elena Vengerova <hizeretta@gmail.com>
2023-11-20 01:12:58 +0200

    header, promo

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../components/header/Header';
+import Promo from '../components/promo/Promo';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <p>Internet Shop</p>
+      <Router>
+        <Header />
+        <Promo />
+      </Router>
     </div>
   );
 }
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,46 @@
+import './Promo.css';
+import PromoImg from '../../assets/images/banners/banner1.png';
+
+function Promo() {
+  return (
+    <section className="promo">
+      <div className="container">
+        <div className="promo_content">
+          <div className="promo_text">
+            <div className="promo_title">FIND CLOTHES THAT MATCHES YOUR STYLE </div>
+            <div className="promo_desc">
+              Browse through our diverse range of meticulously crafted garments, designed to bring
+              out your individuality and cater to your sense of style.
+            </div>
+            <div className="promo_btn-wrapper">
+              <a href="#!" className="promo_btn">
+                Shop Now
+              </a>
+            </div>
+            <div className="promo_list-wrapper">
+              <ul className="promo_list">
+                <li className="promo_list-item">
+                  200+
+                  <p>International Brands</p>
+                </li>
+                <li className="promo_list-item">
+                  2,000+
+                  <p>High-Quality Products</p>
+                </li>
+                <li className="promo_list-item">
+                  30,000+
+                  <p>Happy Customers</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="promo_img">
+            <img src={PromoImg} alt="Promo" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Promo;
