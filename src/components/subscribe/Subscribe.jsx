f39f433608144050df0cc406e94a1a6ca3083e41
venherova <139897339+Venherova@users.noreply.github.com>
2023-12-14 21:09:54 +0200

    subscribe modal (#32)

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,28 +1,46 @@
+import { useState } from 'react';
 import EmailImg from '../../assets/images/icons/email.svg';
+
+import Modal from '../modal/Modal';
 import './Subscribe.css';
 
 function Subscribe() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleSubscribe = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <section className="subscribe">
-      <div className="container">
-        <div className="subscribe_content">
-          <h2 className="subscribe_title">Stay upto date about our latest offers</h2>
-          <div className="subscribe_wrapper">
-            <div className="subscribe_email-wrapper">
-              <div className="subscribe_email-icon">
-                <img src={EmailImg} alt="email" />
-              </div>
-              <input
-                className="subscribe_email"
-                style={{ image: `url(${EmailImg})` }}
-                type="email"
-                placeholder="Enter your email address"
-              />
-            </div>
+      <div>
+        <Modal
+          isOpen={isModalOpen}
+          onCancel={() => setIsModalOpen(false)}
+          onConfirm={() => setIsModalOpen(false)}
+        >
+          Дякуємо, що підписались!
+        </Modal>
 
-            <button className="subscribe_btn" type="button">
-              Subscribe to Newsletter
-            </button>
+        <div className="container">
+          <div className="subscribe_content">
+            <h2 className="subscribe_title">Stay upto date about our latest offers</h2>
+            <div className="subscribe_wrapper">
+              <div className="subscribe_email-wrapper">
+                <div className="subscribe_email-icon">
+                  <img src={EmailImg} alt="email" />
+                </div>
+                <input
+                  className="subscribe_email"
+                  style={{ image: `url(${EmailImg})` }}
+                  type="email"
+                  placeholder="Enter your email address"
+                />
+              </div>
+
+              <button className="subscribe_btn" type="button" onClick={handleSubscribe}>
+                Subscribe to Newsletter
+              </button>
+            </div>
           </div>
         </div>
       </div>
