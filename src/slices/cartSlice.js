df1f428bae0223ff20b2098ce7c34bbe0ae538bf
Victoria Laputko <vikalaputko87@gmail.com>
2023-12-20 23:04:45 +0200

    fixed remarks

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [],
+    items: JSON.parse(localStorage.getItem('userCart')) || [],
   },
   reducers: {
     addToCart: (state, action) => {
@@ -14,10 +14,12 @@ export const cartSlice = createSlice({
       const foundItem = state.items.find((item) => item.itemId === itemId);
       if (foundItem) {
         foundItem.itemQuantity = quantity;
+        localStorage.setItem('userCart', JSON.stringify(state.items));
       }
     },
     removeFromCart: (state, action) => {
       state.items = state.items.filter((item) => item.itemId !== action.payload);
+      localStorage.setItem('userCart', JSON.stringify(state.items));
     },
 
     clearCart: (state) => {
